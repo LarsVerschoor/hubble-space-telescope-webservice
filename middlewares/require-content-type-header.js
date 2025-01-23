d4f9dda8c8d9a388b7c9d9ae48e5df9ed4d1fbda
LarsVerschoor <larsverschoor2@gmail.com>
2025-01-23 15:02:58 +0100

    Fixed Content-Type bug

--- middlewares/require-content-type-header.js
+++ middlewares/require-content-type-header.js
@@ -1,5 +1,5 @@
 const requireContentTypeHeader = (req, res, next) => {
-    if (req.headers['accept'] !== 'application/json' && req.headers['accept'] !== 'application/x-www-form-urlencoded') {
+    if (req.headers['content-type'] !== 'application/json' && req.headers['content-type'] !== 'application/x-www-form-urlencoded') {
         return res.status(415).json({error: 'The Content-Type header must have the value application/json or application/x-www-form-urlencoded'});
     }
     next();
